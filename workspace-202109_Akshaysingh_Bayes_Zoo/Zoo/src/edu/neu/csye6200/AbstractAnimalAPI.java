5db8198fdd1aebd9b01522edc89843e622db89ca
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2021-11-18 00:02:55 -0500

    Delete workspace-202109_Akshaysingh_Bayes_Zoo directory

--- workspace-202109_Akshaysingh_Bayes_Zoo/Zoo/src/edu/neu/csye6200/AbstractAnimalAPI.java
+++
@@ -1,6 +0,0 @@
-package edu.neu.csye6200;
-
-public abstract class AbstractAnimalAPI {
-	
-	public abstract void speak();
-}
--- workspace-202109_Akshaysingh_Bayes_Zoo/Zoo/src/edu/neu/csye6200/AnimalisticAPI.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.csye6200;
-
-public interface AnimalisticAPI {
-	
-	public void speak();
-
-}
